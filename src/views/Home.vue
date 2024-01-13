90a105b65f2532df1705a3137a8aff764dcd471a
Ahmet Hüseyin Dok <off.ahmethuseyindok@gmail.com>
2024-01-13 21:39:38 +0700

    hash router

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 
 <template>
-<div class="h-screen w-screen flex justify-center items-center bg-slate-100">
-    <div class="bg-white p-4 rounded-md">   
-        <h1 class="text-center text-lg font-bold">Vue Dynamic <span>https://github.com/AhmetHuseyinDOK/vue-dynamic-form-starter</span></h1>
-        <ul class="text-center">
-           <li v-for="link in links" ><a class="bg-blue-400 block rounded-sm hover:bg-blue-600 mt-2 p-3 text-blue-950 font-bold" :href="link.href">{{link.title}}</a></li> 
-        </ul>
-        
-        
+    <div class="h-screen w-screen flex justify-center items-center bg-slate-100">
+        <div class="bg-white p-4 rounded-md">
+            <h1 class="text-center text-lg font-bold">Vue Dynamic
+                <span>https://github.com/AhmetHuseyinDOK/vue-dynamic-form-starter</span></h1>
+            <ul class="text-center">
+                <li v-for="link in links"><a
+                        class="bg-blue-400 block rounded-sm hover:bg-blue-600 mt-2 p-3 text-blue-950 font-bold"
+                        :href="link.href">{{ link.title }}</a></li>
+            </ul>
+
+
+        </div>
     </div>
-</div>
 </template>
 
 <script setup lang="ts">
@@ -17,15 +20,15 @@ import { ref } from 'vue';
 
 const links = ref([
     {
-        href: '/vue-dynamic-form-starter' + '/examples/simple-form',
+        href: '/#/examples/simple-form',
         title: 'Login Form'
     },
     {
-        href: '/vue-dynamic-form-starter' + '/examples/complex-form',
+        href: '/#/examples/complex-form',
         title: 'Application Form'
     },
     {
-        href: '/vue-dynamic-form-starter' + '/examples/post-publish',
+        href: '/#/examples/post-publish',
         title: 'Post Publish'
     }
 ]);
